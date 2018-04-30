548396bdce9b91526999d643dc01135f3c4a7182
Maroš Antala <maros.antala@gmail.com>
2018-04-30 19:06:45 +0200

    Made as library. New functions (get and change)

--- script.js
+++ script.js
@@ -1,28 +1,26 @@
-const Cookies = {
-
+const Cookies = (function() {
   // New Error message.
-  error: new Error(),
-
+  var error = new Error();
   /**
    * Get formated time.
    * @param  {integer}    days    Days, which will be transfered to full formated date.
    * @return {string}             String with formated date.
    */
-  getDays: function(days) {
+  var getDays = function(days) {
     var d = new Date();
     d.setTime( d.getTime() + ( days*24*60*60*1000 ) );
     d.toUTCString();
     return d;
-  },
+  }
   /**
    * Simple function for retrieving all cookies.
    * @return    {array}     Array of cookies set in browser.
    */
-  getCookies: function() {
+  var getCookies = function() {
     // Array of cookies
     var cookies = document.cookie.split('; ');
     return cookies;
-  },
+  }
   /**
    * Set a new cookie to browser.
    * @param  {string}     name      Name of new cookie.
@@ -31,51 +29,43 @@ const Cookies = {
    * @param  {url}        page      URL, where cookie will be set.
    * @return {}                     No return, function just create new cookie.
    */
-  setCookie: function(name, value, expire, page) {
-
+  var setCookie = function(name, value, expire, page) {
     this.name       = name;
     this.value      = value;
     this.expire     = expire;
     this.page       = page;
-
-    if(!name && !value) {
-      console.error( this.error.message = 'You didn\'t specified cookie name or value!' );
+    if(!name && !value || !name || !value) {
+      console.error( error.message = 'You didn\'t specified cookie name and value!' );
       return;
     }
-
     if(expire) {
       expire = expire;
     }
     else {
       expire = 3650;
-      expire = this.getDays(expire);
+      expire = getDays(expire);
     }
-
     // BUG: This is not tested, I have not been working with this kind of stuff yet.
     if(!page) {
       page = 'path=/';
     }
-
     document.cookie = name + '=' + value + ';expires=' + expire + ';' + page + ';';
-
-  },
+  }
   /**
    * Remove existing cookie from browser.
    * @param     {string}    name    Name of cookie to delete.
    * @return    {}                  No return, function just delete existing cookie.
    */
-  destroyCookie: function(name) {
-
+  var destroyCookie = function(name) {
     this.name = name;
-
     // Param name is required. No name? No fun!
     if(!name) {
-      console.error( this.error.message = 'You didn\'t specified cookie name!' );
+      console.error( error.message = 'You didn\'t specified cookie name!' );
       return;
     }
     else {
       // Retrieve cookies in arr
-      var cookies   = this.getCookies(),
+      var cookies   = getCookies(),
           loop      = true,
           i         = 0;
       // Loop trough them and find that right cookie.
@@ -84,7 +74,6 @@ const Cookies = {
         var cookie        = cookies[i],
             cookieName    = cookie.split('=')[0],
             cookieValue   = cookie.split('=')[1];
-
         // Got it? Kill it, Anakin.
         if(cookieName === name) {
           document.cookie = cookieName + '=' + cookieValue + ';expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
@@ -94,11 +83,66 @@ const Cookies = {
         else {
           i++;
         }
-
       }
-
     }
+  }
+  /**
+   * Function to change existing cookie.
+   * @param  {string}       name      Name of cookie to change.
+   * @param  {string}       value     New value for cookie.
+   * @return {}                       Delete and create new cookie with new value.
+   */
+  var changeCookie = function(name, value) {
+    this.name     = name;
+    this.value    = value;
+    if(!name && !value || !name || !value) {
+      console.log( error.message = 'You didn\'t specified cookie name or new value!' );
+      return;
+    }
+    else {
+      var cookies = getCookies();
+      for(var i in cookies) {
+        var cookie = cookies[i],
+            cookieName = cookie.split('=')[0],
+            cookieValue = cookie.split('=')[1];
+        if(cookieName === name) {
+          destroyCookie(name);
+          setCookie(name, value);
+        }
+      }
+    }
+  }
+  /**
+   * Get a single cookie with its value.
+   * @param  {string}     name    Name of cookie to get.
+   * @return {string}             String with cookie name and its value.
+   */
+  var getCookie = function(name) {
+    this.name = name;
+    if(!name) {
+      console.log( error.message = 'You didn\'t specified cookie name!' );
+      return;
+    }
+    else {
+      var cookies = getCookies();
+      for(var i in cookies) {
+        var cookie = cookies[i],
+            cookieName = cookie.split('=')[0],
+            cookieValue = cookie.split('=')[1];
+        if(cookieName === name) {
+          return 'Cookie name: ' + cookieName + '\n Cookie value: ' + cookieValue;
+        }
+      }
+    }
+  }
+
+  return {
+
+    setCookie: setCookie,
+    destroyCookie: destroyCookie,
+    changeCookie: changeCookie,
+    getCookie: getCookie
 
   }
 
-}
+}());
